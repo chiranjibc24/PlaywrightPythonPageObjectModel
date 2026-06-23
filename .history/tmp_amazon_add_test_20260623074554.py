93ddc6365e57e32900ea1414506b2fecea61c33b
chiranjibc24 <chiranjib.c24@gmail.com>
2026-06-23 07:49:15 +0530

    feat: Add initial Amazon test script for product search and cart count verification

---
+++ .history/tmp_amazon_add_test_20260623074554.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)  # use False for debugging
+    page = browser.new_page()
+
+    page.goto(
+        "https://www.amazon.in/s?k=iphone+17",
+        wait_until="networkidle",
+        timeout=60000
+    )
+
+    print("Current URL:", page.url)
+    print("Page Title:", page.title())
+
+    page.locator("#nav-cart-count").wait_for(
+        state="visible",
+        timeout=60000
+    )
+
+    print(
+        "Initial cart count:",
+        page.locator("#nav-cart-count").text_content()py
+    )
+
+    browser.close()
